a4f4060425eedba6058c37c68bf46042acfe88b1
Phoenix <phx@amanita.lan>
2018-12-19 12:33:30 +1100

    more model design experiments

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const samplePicsPath = './sample-pics'
 const trainingEpochs = 1
 const batchSize = 2000*4
 const trainingCycles = 7000/4 * 100
-const modelName = 'motionConv2'
+const modelName = 'motionConv4'
 const inputPatchSize = 12 // how big the square tile is that the estimator sees
 const maxMotionEstimate = inputPatchSize / 2 // how big the maximum movement between the two frames is, in pixels, in the auto training set
 const inputShape = [inputPatchSize, inputPatchSize, 6] // 6 channels because two rgb patches are combined
 const modelMultiplier = 1
 
-const saveModelInterval = 10
+const saveModelInterval = 250
 const learningRate = 0.0005
 const optimizer = tf.train.adam(learningRate)
 
--- models.js
+++ models.js
@@ -29,6 +29,18 @@ let modelBuilders = {
     model.add(tf.layers.dense({units: 2, activation: 'linear'}))
     return model
   },
+
+  // what if we can make this really small tho
+  motionDense2: (shape, multiplier)=> {
+    const model = tf.sequential()
+    model.add(tf.layers.flatten({inputShape: shape}))
+    model.add(tf.layers.dense({units: Math.round(shape[0] * shape[1]), activation: 'tanh'}))
+    model.add(tf.layers.dense({units: Math.round(shape[0] * shape[1]), activation: 'tanh'}))
+    model.add(tf.layers.dense({units: Math.round(shape[0] * shape[1]), activation: 'relu'}))
+    model.add(tf.layers.dense({units: 2, activation: 'linear'}))
+    return model
+  },
+
   // high score: bs 250 ~86%; ~92-94% by 15,000 cycles
   motionConv1: (shape, multiplier)=> {
     const model = tf.sequential()
@@ -46,7 +58,7 @@ let modelBuilders = {
     return model
   },
 
-  // 70.6% - needs longer training than most other networks
+  // Model Accuracy on testset: 70.1% - batch 5051
   motionConv2: (shape, multiplier)=> {
     const model = tf.sequential()
     model.add(tf.layers.conv2d({
@@ -65,6 +77,35 @@ let modelBuilders = {
     model.add(tf.layers.dense({units: 2, activation: 'linear'}))
     return model
   },
+
+  // what if softmax would help? it didn't. it was bad
+  motionConv3: (shape, multiplier)=> {
+    const model = tf.sequential()
+    model.add(tf.layers.conv2d({
+      inputShape: shape,
+      filters: 2,
+      kernelSize: 2,
+      activation: 'relu'
+    }))
+    model.add(tf.layers.flatten())
+    // thinking this layer can create a bitmap of probabilities that it aligns with that pixel
+    model.add(tf.layers.dense({units: shape[0]*shape[1], activation: 'tanh'}))
+    // and this can transform that in to x/y
+    model.add(tf.layers.dense({units: 2, activation: 'linear'}))
+    return model
+  },
+
+  // what if the front end is just a tiny conv that can invent something kinda like hsv if it wants
+  // gets to 88% after 50,000 cycles
+  motionConv4: (shape, mul)=> {
+    const model = tf.sequential()
+    model.add(tf.layers.conv2d({inputShape: shape, filters: 6, kernelSize: 1, activation: 'tanh'}))
+    model.add(tf.layers.flatten({inputShape: shape}))
+    model.add(tf.layers.dense({units: Math.round(shape[0] * shape[1]) * 2, activation: 'tanh'}))
+    model.add(tf.layers.dense({units: Math.round(shape[0] * shape[1]), activation: 'tanh'}))
+    model.add(tf.layers.dense({units: 2, activation: 'linear'}))
+    return model
+  }
 }
 
 module.exports = modelBuilders;
